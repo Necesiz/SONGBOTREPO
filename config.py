5cc26fcd54ded50f8620a9a24f7cf7caababb207
Necesiz <97000998+Necesiz@users.noreply.github.com>
2022-12-12 02:53:34 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_OWNER = os.environ.get("BOT_OWNER")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME")
+    BOT_TOKEN = os.environ.get("5681957815:AAGzVJmtl_LH7Vs-MfgbcjP9Hh-y0wHJrb0")
+    API_ID = int(os.environ.get("17568815"))
+    API_HASH = os.environ.get("177622d39f23e7c3d015f3d6ebaacd31")
+    BOT_OWNER = os.environ.get("abasof_015")
+    BOT_USERNAME = os.environ.get("AzeSongRobot")
+    PLAYLIST_NAME = os.environ.get("azesongplaylist")
     GROUP = os.environ.get("GROUP") 
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
+    PLAYLIST_ID = int(os.environ.get("-1001877177311"))
